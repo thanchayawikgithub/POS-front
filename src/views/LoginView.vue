a9245de32ac3d88c1d2ae711fb32ab57c65acb6e
Anawin Wiriyasathitkul <64160150@go.buu.ac.th>
2022-12-23 17:02:13 +0700

    create validate

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,20 +1,54 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import type { VForm } from "vuetify/components";
 const loginName = ref("");
 const Password = ref("");
+const valid = ref(true);
+const form = ref<InstanceType<typeof VForm> | null>(null);
+const login = async () => {
+  const { valid } = await form.value!.validate();
+  if (valid) {
+    console.log("Form Success");
+  }
+};
+const reset = () => {
+  form.value?.reset();
+};
 </script>
 <template>
   <v-app>
     <v-main class="pa-16 bg-blue-grey-darken-2">
       <v-card width="400px" class="mx-auto">
-        <v-card-title primary-title class="text-center"> Login </v-card-title>
+        <v-card-title
+          primary-title
+          class="text-center text-h6 font-weight-bold"
+        >
+          Login
+        </v-card-title>
         <v-card-text>
-          <v-form>
-            <v-text-field label="Login Name" v-model="loginName"></v-text-field>
+          <v-form ref="form" v-model="valid">
+            <v-text-field
+              label="Login Name"
+              v-model="loginName"
+              :rules="[
+                (v) => !!v || 'Login Name is required',
+                (v) =>
+                  v.length >= 4 ||
+                  'Name must be more than or equal 4 characters',
+              ]"
+              required
+            ></v-text-field>
             <v-text-field
               label="Password"
               type="Password"
               v-model="Password"
+              :rules="[
+                (v) => !!v || 'Password is required',
+                (v) =>
+                  v.length >= 8 ||
+                  'Password must be more than or equal 8 characters',
+              ]"
+              required
             ></v-text-field>
           </v-form>
           <pre>
@@ -23,8 +57,8 @@ const Password = ref("");
           </pre>
         </v-card-text>
         <v-card-actions class="justify-center">
-          <v-btn color="success">Login</v-btn>
-          <v-btn color="error">Clear</v-btn>
+          <v-btn color="success" @click="login">Login</v-btn>
+          <v-btn color="error" @click="reset">Clear</v-btn>
         </v-card-actions>
       </v-card>
     </v-main>
