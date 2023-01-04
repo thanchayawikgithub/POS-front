49159798139486cff0b692fc62bd1bbc4417929d
Than Chayawik <64160244@go.buu.ac.th>
2023-01-04 21:16:26 +0700

    Add initial menu component

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -2,9 +2,8 @@
 export default {
   data() {
     return {
-      tab: "Appetizers",
-      items: ["Appetisers", "Drink", "Deserts", "Cocktails"],
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      tab: "Menu",
+      items: ["Drink", "Bakery", "Food"],
     };
   },
 };
@@ -12,24 +11,29 @@ export default {
 
 <template>
   <v-container style="padding: 0">
-    <v-card flat>
-      <v-card-title class="text-center justify-center py-6">
-        <h1 class="font-weight-bold text-h2 text-basil">Menu</h1>
-      </v-card-title>
+    <v-row>
+      <v-col cols="8">
+        <v-card flat>
+          <v-card-title class="text-center justify-center py-6">
+            <h1 class="font-weight-bold text-h2 text-basil">Menu</h1>
+          </v-card-title>
 
-      <v-tabs v-model="tab" bg-color="transparent" color="basil" grow>
-        <v-tab v-for="item in items" :key="item" :value="item">
-          {{ item }}
-        </v-tab>
-      </v-tabs>
+          <v-tabs v-model="tab" grow>
+            <v-tab v-for="item in items" :key="item" :value="item">
+              {{ item }}
+            </v-tab>
+          </v-tabs>
 
-      <v-window v-model="tab">
-        <v-window-item v-for="item in items" :key="item" :value="item">
-          <v-card color="basil" flat>
-            <v-card-text>{{ text }}</v-card-text>
-          </v-card>
-        </v-window-item>
-      </v-window>
-    </v-card>
+          <v-window v-model="tab">
+            <v-window-item v-for="item in items" :key="item" :value="item">
+              <v-card flat>
+                <v-card-item>{{ item }}</v-card-item>
+              </v-card>
+            </v-window-item>
+          </v-window>
+        </v-card>
+      </v-col>
+      <v-col></v-col>
+    </v-row>
   </v-container>
 </template>
