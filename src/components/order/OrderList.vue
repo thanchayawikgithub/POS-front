5471e8bd723d6a8ab36209a8bfdc319af35dce6b
Jettada Kongchai <64160044@go.buu.ac.th>
2023-01-06 18:05:56 +0700

    Order List Table

--- src/components/order/OrderList.vue
+++ src/components/order/OrderList.vue
@@ -3,34 +3,59 @@ import { mdiCoffee } from "@mdi/js";
 </script>
 <template>
   <div>
-    <v-container>
-      <v-row>
-        <v-table>
-          <thead>
-            <th Class="text-left">
-              <v-icon :class="mdiCoffee"></v-icon> Order List
-            </th>
-            <th></th>
-          </thead>
-          <tbody>
-            <td></td>
-            <td></td>
-          </tbody>
-        </v-table>
-      </v-row>
-      <v-row>
-        <v-col> Member: </v-col>
-        <v-col> </v-col>
-      </v-row>
-      <v-row>
-        <v-col> Telephone Number: </v-col>
-        <v-col> </v-col>
-      </v-row>
-      <v-row>
-        <v-col> Number Of Points: </v-col>
-        <v-col> </v-col>
-      </v-row>
-    </v-container>
+    <v-table>
+      <thead>
+        <tr>
+          <th class="text-left"></th>
+          <th class="text-left"></th>
+          <th class="text-right"></th>
+          <th class="text-center"></th>
+          <th class="text-left"></th>
+          <th class="text-left">Order</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <v-img height="50px" width="50px" src="mocca.jpg"></v-img>
+          </td>
+          <td>Mocca</td>
+          <td>
+            <v-card-actions class="justify-center">
+              <v-btn color="error"> - </v-btn>
+            </v-card-actions>
+          </td>
+          <td class="text-center">1</td>
+          <td>
+            <v-card-actions class="justify-left">
+              <v-btn color="secondary"> + </v-btn>
+            </v-card-actions>
+          </td>
+
+          <td class="text-center">60</td>
+        </tr>
+        <tr>
+          <td>
+            <v-img height="50px" width="50px" src="americano.jpg"></v-img>
+          </td>
+          <td>Amaricano</td>
+          <td>
+            <v-card-actions class="justify-center">
+              <v-btn color="error"> - </v-btn>
+            </v-card-actions>
+          </td>
+          <td class="text-center">1</td>
+          <td>
+            <v-card-actions class="justify-left">
+              <v-btn color="secondary"> + </v-btn>
+            </v-card-actions>
+          </td>
+
+          <td class="text-center">70</td>
+        </tr>
+      </tbody>
+    </v-table>
   </div>
+
   <div></div>
 </template>
