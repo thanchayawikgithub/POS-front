92796e07c0d69ea546515265babffce4016f6154
Jettada Kongchai <64160044@go.buu.ac.th>
2023-01-06 18:05:39 +0700

    Order List Table

---
+++ src/components/order/OrderList.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { mdiCoffee } from "@mdi/js";
+</script>
+<template>
+  <div>
+    <v-container>
+      <v-row>
+        <v-table>
+          <thead>
+            <th Class="text-left">
+              <v-icon :class="mdiCoffee"></v-icon> Order List
+            </th>
+            <th></th>
+          </thead>
+          <tbody>
+            <td></td>
+            <td></td>
+          </tbody>
+        </v-table>
+      </v-row>
+      <v-row>
+        <v-col> Member: </v-col>
+        <v-col> </v-col>
+      </v-row>
+      <v-row>
+        <v-col> Telephone Number: </v-col>
+        <v-col> </v-col>
+      </v-row>
+      <v-row>
+        <v-col> Number Of Points: </v-col>
+        <v-col> </v-col>
+      </v-row>
+    </v-container>
+  </div>
+  <div></div>
+</template>
