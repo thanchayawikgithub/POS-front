e6d6bb455e84d5c6c38a7c29f887ca5a396fca9c
Anawin Wiriyasathitkul <64160150@go.buu.ac.th>
2023-01-07 23:32:07 +0700

    Add Select and Fix Search Bar

--- src/components/order/TotalMoney.vue
+++ src/components/order/TotalMoney.vue
@@ -1,27 +1,25 @@
 <script setup lang="ts"></script>
 <template>
-  <v-table>   
+  <v-table>
     <tbody>
-        <tr>
-            <td>Member :</td>
-            <td class="">12345678</td>
-        </tr>
-        <tr>
-            <td>Price :</td>
-            <td>130</td>
-        </tr>
-        <tr>
-            <td>Discount :</td>
-            <td>5</td>
-        </tr>       
-        <tr>
-            <td>Total :</td>
-            <td>125</td>
-        </tr>
+      <tr>
+        <td>Member :</td>
+        <td class="">12345678</td>
+      </tr>
+      <tr>
+        <td>Price :</td>
+        <td>130</td>
+      </tr>
+      <tr>
+        <td>Discount :</td>
+        <td>5</td>
+      </tr>
+      <tr>
+        <td>Total :</td>
+        <td>125</td>
+      </tr>
     </tbody>
   </v-table>
   <div></div>
 </template>
-<style scoped>
-
-</style>
+<style scoped></style>
