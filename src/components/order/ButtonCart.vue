26efc3abc4c7fa13853dfcbe7461f624e90d6289
Kitiwat Daowrueng <64160131@go.buu.ac.th>
2023-01-07 22:54:16 +0700

    Rotate Btn to Top table

--- src/components/order/ButtonCart.vue
+++ src/components/order/ButtonCart.vue
@@ -2,22 +2,8 @@
 
 <template>
   <v-container>
-    <v-row>
-      <v-col
-        ><v-btn class="justify-center" width="100%" variant="outlined" rounded>
-          Pay
-        </v-btn></v-col
-      >
-      <v-col
-        ><v-btn class="justify-center" width="100%" variant="outlined" rounded>
-          Register
-        </v-btn></v-col
-      >
-      <v-col
-        ><v-btn class="justify-center" width="100%" variant="outlined" rounded>
-          Search
-        </v-btn></v-col
-      >
+    <v-row class="text-center">
+      <v-col><v-btn width="50%" variant="outlined" rounded> Pay </v-btn></v-col>
     </v-row>
   </v-container>
 </template>
--- src/components/order/TotalMoney.vue
+++ src/components/order/TotalMoney.vue
@@ -1,27 +1,43 @@
 <script setup lang="ts"></script>
 <template>
-  <v-table>   
-    <tbody>
+  <div class="tab">
+    <v-container>
+      <v-row class="text-center">
+        <v-col
+          ><v-btn width="100%" variant="outlined" rounded>
+            Register Member
+          </v-btn>
+        </v-col>
+        <v-col>
+          <v-btn width="100%" variant="outlined" rounded> Search </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-table>
+      <tbody>
         <tr>
-            <td>Member :</td>
-            <td class="">12345678</td>
+          <td>Member :</td>
+          <td class="">12345678</td>
         </tr>
         <tr>
-            <td>Price :</td>
-            <td>130</td>
+          <td>Price :</td>
+          <td>130</td>
         </tr>
         <tr>
-            <td>Discount :</td>
-            <td>5</td>
-        </tr>       
-        <tr>
-            <td>Total :</td>
-            <td>125</td>
+          <td>Discount :</td>
+          <td>5</td>
         </tr>
-    </tbody>
-  </v-table>
+        <tr>
+          <td>Total :</td>
+          <td>125</td>
+        </tr>
+      </tbody>
+    </v-table>
+  </div>
   <div></div>
 </template>
 <style scoped>
-
+.tab {
+  margin-top: 90px;
+}
 </style>
