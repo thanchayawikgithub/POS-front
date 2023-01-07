0fea664d937a96fba13666627ee6543b0c25bf85
Pattarapol Chaijam <64160249@go.buu.ac.thcls>
2023-01-08 02:13:14 +0700

    Edit Login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -22,48 +22,82 @@ const reset = () => {
 </script>
 <template>
   <v-app>
-    <v-main class="pa-16 bg-blue-grey-darken-2">
-      <v-card width="400px" class="mx-auto">
-        <v-card-title
-          primary-title
-          class="text-center text-h6 font-weight-bold"
+    <v-card>
+      <v-img
+        src="https://img.freepik.com/premium-vector/seamless-background-hand-drawn-coffee-equipment_108231-235.jpg?w=1380"
+      >
+        <v-card
+          width="1000px"
+          height="500"
+          class="mx-auto pa-16 ma-16"
+          style="flex: 100px"
+          color="#413839"
         >
-          Login
-        </v-card-title>
-        <v-card-text>
-          <v-form ref="form" v-model="valid">
-            <v-text-field
-              label="Login Name"
-              v-model="loginName"
-              :rules="[
-                (v) => !!v || 'Login Name is required',
-                (v) =>
-                  v.length >= 4 ||
-                  'Name must be more than or equal 4 characters',
-              ]"
-              required
-            ></v-text-field>
-            <v-text-field
-              label="Password"
-              type="Password"
-              v-model="password"
-              :rules="[
-                (v) => !!v || 'Password is required',
-                (v) =>
-                  v.length >= 8 ||
-                  'Password must be more than or equal 8 characters',
-              ]"
-              required
-            ></v-text-field>
-          </v-form>
-          <pre></pre>
-        </v-card-text>
-        <v-card-actions class="justify-center">
-          <v-btn color="success" @click="login">Login</v-btn>
-          <v-btn color="error" @click="reset">Clear</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-main>
+          <v-row>
+            <v-col cols="6"> </v-col>
+            <v-col cols="6">
+              <v-card-title
+                primary-title
+                class="text-center text-h6 font-weight-bold"
+              >
+                Login
+              </v-card-title>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col clos="6">
+              <v-img
+                class="align-end"
+                height="300px"
+                src="https://i.pinimg.com/564x/0c/6a/78/0c6a788a868ad6793bbc56c9a4e1b59b.jpg"
+                cover
+              />
+            </v-col>
+
+            <v-col cols="6">
+              <v-card-text>
+                <v-form ref="form" v-model="valid">
+                  <v-text-field
+                    label="Login Name"
+                    v-model="loginName"
+                    :rules="[
+                      (v) => !!v || 'Login Name is required',
+                      (v) =>
+                        v.length >= 4 ||
+                        'Name must be more than or equal 4 characters',
+                    ]"
+                    required
+                  ></v-text-field>
+                  <v-text-field
+                    label="Password"
+                    type="Password"
+                    v-model="password"
+                    :rules="[
+                      (v) => !!v || 'Password is required',
+                      (v) =>
+                        v.length >= 8 ||
+                        'Password must be more than or equal 8 characters',
+                    ]"
+                    required
+                  ></v-text-field>
+                </v-form>
+
+                <pre></pre>
+              </v-card-text>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col clos="6" />
+            <v-col cols="6">
+              <v-card-actions class="justify-center">
+                <v-btn color="success" @click="login">Login</v-btn>
+                <v-btn color="error" @click="reset">Clear</v-btn>
+              </v-card-actions>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-img>
+    </v-card>
   </v-app>
 </template>
 <style scoped></style>
