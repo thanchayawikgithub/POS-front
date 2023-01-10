e336791e85536a9c2b73266ed424ae93a767fbdf
Jettada Kongchai <64160044@go.buu.ac.th>
2023-01-10 10:33:48 +0700

    Delete TabTest

--- src/views/ShowName.vue
+++ src/views/ShowName.vue
@@ -1,21 +1,5 @@
-<script setup lang="ts">
-// import BenzComponent from "@/components/BenzComponent.vue";
-// import TanComponents from "@/components/TanComponents.vue";
-// import TeeComponents from "@/components/TeeComponents.vue";
-// import ThanComponent from "@/components/ThanComponent.vue";
-// import WinComponent from "@/components/WinComponent.vue";
-// import TongComponent from "@/components/TongComponent.vue";
-// import YutaComponent from "@/components/YutaComponent.vue";
-import TabComponent from "@/components/TabComponent.vue";
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <!-- <BenzComponent />
-  <TanComponents />
-  <TeeComponents />
-  <ThanComponent />
-  <WinComponent />
-  <TongComponent />
-  <YutaComponent /> -->
-  <TabComponent></TabComponent>
+  <div></div>
 </template>
