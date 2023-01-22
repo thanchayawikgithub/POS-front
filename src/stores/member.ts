288d0057ad09c5a016932cab3bbeac4443728d33
Than Chayawik <64160244@go.buu.ac.th>
2023-01-22 18:17:19 +0700

    Create member  store

---
+++ src/stores/member.ts
@@ -0,0 +1,11 @@
+import { ref } from "vue";
+import { defineStore } from "pinia";
+import type Member from "@/types/member";
+
+export const useCounterStore = defineStore("member", () => {
+  const memberList = ref<Member[]>([]);
+  const registerMember = (member: Member) => {
+    memberList.value.push(member);
+  };
+  return { memberList, registerMember };
+});
---
+++ src/types/Member.ts
@@ -0,0 +1,5 @@
+export default interface Member {
+  id: number;
+  name: string;
+  phone: string;
+}
