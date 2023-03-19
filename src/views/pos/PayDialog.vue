7b75539a6766647a0745508ee12272a3f0ae68f7
Pattarapol Chaijam <64160249@go.buu.ac.thcls>
2023-03-19 21:41:12 +0700

    UpdatePayDialog and SuccessDialog

--- src/views/pos/PayDialog.vue
+++ src/views/pos/PayDialog.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { useOrderStore } from "@/stores/order";
-
+import { mdiQrcodeScan, mdiCashMultiple } from "@mdi/js";
 const orderStore = useOrderStore();
 </script>
 
@@ -13,23 +13,39 @@ const orderStore = useOrderStore();
       <v-container>
         <v-row>
           <v-col cols="12" align="center">
-            <v-btn @click="orderStore.paymentMethod = 'QR-Payment'"
+            <v-btn
+              class="mx-2"
+              :prepend-icon="mdiQrcodeScan"
+              @click="orderStore.paymentMethod = 'QR-Payment'"
               >QR-Payment</v-btn
             >
-            <v-btn @click="orderStore.paymentMethod = 'Cash'">Cash</v-btn>
+            <v-btn
+              :prepend-icon="mdiCashMultiple"
+              @click="orderStore.paymentMethod = 'Cash'"
+              >Cash</v-btn
+            >
           </v-col>
         </v-row>
       </v-container>
 
       <v-expand-transition>
         <v-container v-if="orderStore.paymentMethod === 'Cash'"
-          ><v-row>Total : {{ orderStore.totalPrice }} Baht</v-row>
-          <v-row>Received</v-row>
-          <v-row>
-            <v-text-field
-              v-model="orderStore.received"
-              hint="Received"
-            ></v-text-field>
+          ><v-row
+            ><v-col align="center" style="font-size: 21px"
+              >Total : {{ orderStore.totalPrice }} Baht</v-col
+            ></v-row
+          >
+          <v-row
+            ><v-col style="font-size: 21px" cols="3" class="pa-6"
+              >Received :</v-col
+            >
+            <v-col cols="6">
+              <v-text-field
+                v-model="orderStore.received"
+                hint="Received"
+                prefix="฿"
+              ></v-text-field>
+            </v-col>
           </v-row>
           <v-row class="justify-center"
             ><v-btn
@@ -44,7 +60,10 @@ const orderStore = useOrderStore();
       </v-expand-transition>
       <v-expand-transition>
         <v-container v-if="orderStore.paymentMethod === 'QR-Payment'"
-          ><v-row>Total : {{ orderStore.totalPrice }} Baht</v-row
+          ><v-row
+            ><v-col align="center" style="font-size: 21px"
+              >Total : {{ orderStore.totalPrice }} Baht</v-col
+            ></v-row
           ><v-img src="./public/QR.jpg" height="300px"></v-img
         ></v-container>
       </v-expand-transition>
--- src/views/pos/SuccessDialog.vue
+++ src/views/pos/SuccessDialog.vue
@@ -4,10 +4,25 @@ import { useOrderStore } from "@/stores/order";
 const orderStore = useOrderStore();
 </script>
 <template>
-  <v-dialog v-model="orderStore.successDialog">
-    <v-card width="40vw" height="40vh"
-      >Success<v-card-actions
-        ><v-btn @click="orderStore.successDialog = false"
+  <v-dialog v-model="orderStore.successDialog" persistent width="600">
+    <v-card width="40vw" height="45vh">
+      <v-container>
+        <v-row>
+          <v-col cols="12" align="center" style="font-size: 35px"
+            >Success
+          </v-col>
+        </v-row>
+        <v-row><v-img src="./public/cc.png" height="100px"></v-img></v-row>
+        <v-row>
+          <v-col cols="12" align="center" style="font-size: 40px"
+            >Change :{{ orderStore.changed }}
+          </v-col>
+        </v-row>
+      </v-container>
+
+      <v-card-actions
+        ><v-spacer></v-spacer
+        ><v-btn color="blue-darken-1" @click="orderStore.successDialog = false"
           >close</v-btn
         ></v-card-actions
       ></v-card
