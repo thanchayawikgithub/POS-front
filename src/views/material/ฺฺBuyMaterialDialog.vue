ce0bedc75d8caefac94dd47859da46896e10ed25
Anawin Wiriyasathitkul <64160150@go.buu.ac.th>
2023-03-22 20:25:46 +0700

    Add combobox Buy mat

--- src/views/material/ฺฺBuyMaterialDialog.vue
+++ src/views/material/ฺฺBuyMaterialDialog.vue
@@ -8,11 +8,26 @@ const vendorStore = useVendorStore();
     ><v-card
       ><v-card-title>Buy Material</v-card-title
       ><v-card-text
-        ><v-container
-          ><v-row
-            v-for="vendor of vendorStore.vendorMats"
-            :key="vendor.vendor_id"
-            >{{ vendor.vendor_name }}</v-row
+        ><v-container style="background-color: antiquewhite"
+          ><v-row>
+            <v-col
+              ><v-row
+                v-for="vendor of vendorStore.vendorMats"
+                :key="vendor.vendor_id"
+                >{{ vendor.vendor_name }}
+                <v-combobox
+                  label=""
+                  :items="vendor.vendorMat"
+                  item-title="v_mat_name"
+                  item-value="v_mat_id"
+                  variant="underlined"
+                ></v-combobox
+              ></v-row>
+            </v-col>
+            <v-col
+              ><v-row><v-col>AA</v-col></v-row>
+              <v-row><v-col>BB</v-col></v-row></v-col
+            ></v-row
           ></v-container
         ></v-card-text
       ></v-card
